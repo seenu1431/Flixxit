c65971ded641b5c68e9f0af23f2ab52bef69e52b
Srinivasan <sv7632512@gmail.com>
2024-04-24 21:56:56 +0530

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
 */
 
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
