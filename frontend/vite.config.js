2fbd877ebef31125e3756e1826776d5227610c7b
Srinivasan <sv7632512@gmail.com>
2024-04-24 21:50:59 +0530

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+*/
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    // include: ['linked-dep'],
+  },
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      // include: [/linked-dep/, /node_modules/],
+    },
+  },
+});
